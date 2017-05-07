6bdb7f5187a1322c1b38b16cb513c839a2075a11
Ahmet Parmaksızoğlu <ahmetpar1@hotmail.com>
2017-05-07 15:33:39 +0300

    Add files via upload

--- passcre.py
+++ passcre.py
@@ -1,10 +1,45 @@
 #!/usr/bin/python3
 import random # random modülü
-bh = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
-kh = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
-se = [".",":",";","_","-","=","+",",","/"]
+
+try :
+    sayı = int(input("kaç haneli şifre olsun >> "))
+except (ValueError):
+    print("!!! LÜTFEN SAYI GİRİNİZ !!!")
+    input("DEVAM ETMEK İÇİN ENTER TUŞUNA BASIN")
+    exit()
+
+buyuhharf = input("büyük harf olsun mu?(E/H)")
+kucukharf = input("küçük harf olsun mu?(E/H)")
+sorusayı = input("sayı olsun mu?(E/H)")
+nokta =input("noktalama olsun mu?(E/H)")
+
+# büyük harfin koşulu
+if (buyuhharf == "e" or buyuhharf == "E"):
+    bh = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V","W", "X", "Y", "Z"]
+else:
+    bh = [""]
+
+# küçük harfin koşulu
+if (kucukharf == "e" or kucukharf == "E"):
+    kh = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v",
+          "w", "x", "y", "z"]
+else:
+    kh = [""]
+
+# satı
+if (sorusayı == "e" or sorusayı == "E"):
+    sa = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
+else:
+    sa = [""]
+
+# nokta
+if (nokta == "e" or nokta == "E"):
+    se = [".", ":", ";", "_", "-", "=", "+", ",", "/"]
+else:
+    se = [""]
+
 Sifre = ""
-sayı = int(input("kaç haneli şifre olsun >> "))
+
 while(True):
     t = len(Sifre)
     if sayı != t:
@@ -14,14 +49,14 @@ while(True):
         elif r == 2:
             Sifre += random.choice(kh)
         elif r == 3:
-            Sifre += str(random.randint(0,9))
+            Sifre += random.choice(sa)
         elif r == 4:
             Sifre += random.choice(se)
         else:
             continue
     elif sayı == t:
         print(Sifre)
-        pause = input("DEVAM ETMEK İÇİN ENTER TUŞUNA BASIN")
+        input("DEVAM ETMEK İÇİN ENTER TUŞUNA BASIN")
         exit()
     else:
         exit()
